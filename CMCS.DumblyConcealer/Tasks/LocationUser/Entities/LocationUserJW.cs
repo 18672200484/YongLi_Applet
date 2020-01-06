2f198002da6bf3b5ede205c9a9b0b09fa8a6da15
刘凡 <fengzhongdeyuyi@hotmail.com>
2020-01-06 11:13:05 +0800

    修改人员定位接口

---
+++ CMCS.DumblyConcealer/Tasks/LocationUser/Entities/LocationUserJW.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMCS.DumblyConcealer.Tasks.LocationUser.Entities.LocationUserJW
+{
+	/// <summary>
+	/// 人员经纬度请求结果
+	/// </summary>
+	public class LocationUserJW
+	{
+		/// <summary>
+		/// 0表示正常，1表示异常
+		/// </summary>
+		public string status { get; set; }
+
+		/// <summary>
+		/// 调用结果提示信息
+		/// </summary>
+		public string msginfo { get; set; }
+
+		public List<data> data { get; set; }
+	}
+	public class data
+	{
+		/// <summary>
+		/// 员工姓名
+		/// </summary>
+		public string empName { get; set; }
+		/// <summary>
+		/// 员工工号 访客、承包商无工号，null
+		/// </summary>
+		public string empNo { get; set; }
+		/// <summary>
+		/// 设备编号（Mac地址）
+		/// </summary>
+		public string deviceNo { get; set; }
+		/// <summary>
+		/// 时间
+		/// </summary>
+		public DateTime dateTime { get; set; }
+		/// <summary>
+		/// X轴坐标
+		/// </summary>
+		public double crossX { get; set; }
+		/// <summary>
+		/// Y轴坐标
+		/// </summary>
+		public double crossY { get; set; }
+		/// <summary>
+		/// 人员卡类型 0:员工，1:访客，2:承包商
+		/// </summary>
+		public string specifictype { get; set; }
+		/// <summary>
+		/// 楼层数
+		/// </summary>
+		public string layer { get; set; }
+		/// <summary>
+		/// 区域id 0为其它区域
+		/// </summary>
+		public string area { get; set; }
+		/// <summary>
+		/// 轨迹信息
+		/// </summary>
+		public List<astarPointList> astarPointList { get; set; }
+	}
+
+	public class astarPointList
+	{
+		public double x { get; set; }
+
+		public double y { get; set; }
+
+		/// <summary>
+		/// 经度
+		/// </summary>
+		public double longitude { get; set; }
+
+		/// <summary>
+		/// 维度
+		/// </summary>
+		public double latitude { get; set; }
+	}
+}
--- CMCS.DumblyConcealer/Tasks/LocationUser/LocationUserDao.cs
+++ CMCS.DumblyConcealer/Tasks/LocationUser/LocationUserDao.cs
@@ -6,6 +6,7 @@ using CMCS.Common.Utilities;
 using CMCS.Common.Utilities.InfluxDBs;
 using CMCS.Common.Utilities.RedisLib;
 using CMCS.DumblyConcealer.Tasks.LocationUser.Entities;
+using CMCS.DumblyConcealer.Tasks.LocationUser.Entities.LocationUserJW;
 using InfluxData.Net.InfluxDb.Models;
 using Newtonsoft.Json;
 using System;
@@ -33,7 +34,7 @@ namespace CMCS.DumblyConcealer.Tasks.LocationUser
 			//获取定位人员信息
 			var json = UtilHttpPost.PostWebApi("", commonDAO.appConfig.Locationlicence + "?username=" + commonDAO.appConfig.LocationUserName + "&password=" + commonDAO.appConfig.LocationPassWord);
 
-			var result = JsonConvert.DeserializeObject<LocationUserResult>(json);
+			var result = JsonConvert.DeserializeObject<LocationUserJW>(json);
 			if (result.status == "0")
 				output("接口调用正常", eOutputType.Normal);
 			else if (result.status == "1")
@@ -41,25 +42,21 @@ namespace CMCS.DumblyConcealer.Tasks.LocationUser
 				output("接口调用异常,异常信息:" + result.msginfo, eOutputType.Warn);
 				return res;
 			}
-			//原点经纬度
-			double lon = commonDAO.appConfig.Xcoor;
-			double lat = commonDAO.appConfig.Ycoor;
-			//左下角经纬度
-			double leftDownx = commonDAO.appConfig.LeftDownXcoor;
-			double leftDowny = commonDAO.appConfig.LeftDownYcoor;
-			//右上角经纬度
-			double rightUpx = commonDAO.appConfig.RightUpXcoor;
-			double rightUpy = commonDAO.appConfig.RightUpYcoor;
+			////原点经纬度
+			//double lon = commonDAO.appConfig.Xcoor;
+			//double lat = commonDAO.appConfig.Ycoor;
+			////左下角经纬度
+			//double leftDownx = commonDAO.appConfig.LeftDownXcoor;
+			//double leftDowny = commonDAO.appConfig.LeftDownYcoor;
+			////右上角经纬度
+			//double rightUpx = commonDAO.appConfig.RightUpXcoor;
+			//double rightUpy = commonDAO.appConfig.RightUpYcoor;
 
-			//左边框长度
-			double leftLength = MissionPlanner.GetDistance(lat, lon, leftDowny, leftDownx);
+			////左边框长度
+			//double leftLength = MissionPlanner.GetDistance(lat, lon, leftDowny, leftDownx);
 
-			//上边框框长度
-			double upLength = MissionPlanner.GetDistance(lat, lon, rightUpy, rightUpx);
-
-			#region 直接进业务库相关
-
-			#endregion
+			////上边框框长度
+			//double upLength = MissionPlanner.GetDistance(lat, lon, rightUpy, rightUpx);
 
 			RedisHelper redis = new RedisHelper(commonDAO.appConfig.UserRedisDatabase);
 			InfluxDBSdkHelper client = new InfluxDBSdkHelper(commonDAO.appConfig.InfluxDbUrlAndPort, commonDAO.appConfig.InfluxDbUser, commonDAO.appConfig.InfluxDbPwd);
@@ -82,69 +79,71 @@ namespace CMCS.DumblyConcealer.Tasks.LocationUser
 				foreach (var item in result.data)
 				{
 					#region 坐标换算
-					ST_GPS_POINT gps = new ST_GPS_POINT();
-					//var xlength = item.crossX / 1000;
-					//var ylength = item.crossY / 1000;
-					var xlength = item.crossX / 10;
-					var ylength = item.crossY / 10;
-					//如果人员定位系统的X坐标大于地图X坐标长度
-					if (xlength > upLength)
-					{
-						xlength = Convert.ToInt32(upLength - commonDAO.appConfig.TempLength);
-					}
-					//如果人员定位系统的Y坐标大于地图Y坐标长度
-					if (ylength > leftLength)
-					{
-						ylength = Convert.ToInt32(leftLength - commonDAO.appConfig.TempLength);
-					}
-					//如果人员的Y坐标小于设定值，强行增加多少米
-					if (ylength < commonDAO.appConfig.TempLength2)
-					{
-						ylength = Convert.ToInt32(ylength + commonDAO.appConfig.TempLength);
-					}
-					gps.sgp_lon = lon + 0.00001141 * xlength;
-					gps.sgp_lat = lat - (0.00000899 * ylength);
+					//ST_GPS_POINT gps = new ST_GPS_POINT();
+					////var xlength = item.crossX / 1000;
+					////var ylength = item.crossY / 1000;
+					//var xlength = item.crossX / 10;
+					//var ylength = item.crossY / 10;
+					////如果人员定位系统的X坐标大于地图X坐标长度
+					//if (xlength > upLength)
+					//{
+					//	xlength = Convert.ToInt32(upLength - commonDAO.appConfig.TempLength);
+					//}
+					////如果人员定位系统的Y坐标大于地图Y坐标长度
+					//if (ylength > leftLength)
+					//{
+					//	ylength = Convert.ToInt32(leftLength - commonDAO.appConfig.TempLength);
+					//}
+					////如果人员的Y坐标小于设定值，强行增加多少米
+					//if (ylength < commonDAO.appConfig.TempLength2)
+					//{
+					//	ylength = Convert.ToInt32(ylength + commonDAO.appConfig.TempLength);
+					//}
+					//gps.sgp_lon = lon + 0.00001141 * xlength;
+					//gps.sgp_lat = lat - (0.00000899 * ylength);
 					#endregion
-
-					//#region 直接进业务库
 
 					////通过信标地址查询人员
-					var usermodel = commonDAO.SelfDber.Entity<JCXXTBCOMPANYUSERINFOEXTEND>("where BEACON=:BEACON", new { BEACON = item.deviceNo });
-					Staffduty_Position staffduty_Position = new Staffduty_Position();
-					staffduty_Position.BuildId = item.area;
-					staffduty_Position.FloorNo = item.layer;
-					staffduty_Position.Name = item.empName;
-					staffduty_Position.Xcoor = gps.sgp_lon;
-					staffduty_Position.Ycoor = gps.sgp_lat;
-					staffduty_Position.Zcoor = commonDAO.appConfig.Zcoor;
-					staffduty_Position.LastUpdateTime = DateTime.Now;
-					staffduty_Position.Id = usermodel == null ? "" : usermodel.UserId;
-
-					Dbers.GetInstance().SelfDber.Insert(staffduty_Position);
-					//#endregion
-
-					#region 存redis数据库
-					redis.HashSet(item.empName, "name", item.empName);
-					redis.HashSet(item.empName, "lastUpdateTime", DateTimeOffset.UtcNow.ToLocalTime());
-					redis.HashSet(item.empName, "x-Coor", gps.sgp_lon);
-					redis.HashSet(item.empName, "y-Coor", gps.sgp_lat);
-					redis.HashSet(item.empName, "floorNo", item.layer);
-					redis.HashSet(item.empName, "buildId", item.area);
-					redis.HashSet(item.empName, "status", "");
-					#endregion
-
-					#region 存时序数据库
-					var point_model = new Point()
+					if (item.astarPointList != null && item.astarPointList.Count() > 0)
 					{
-						Name = commonDAO.appConfig.InfluxDbLocationUserTableName,//表名
-						Tags = new Dictionary<string, object>() { { "userId", item.empName } },
-						Fields = new Dictionary<string, object>() { { "x-Coor", gps.sgp_lon }, { "y-Coor", gps.sgp_lat }, { "floorNo", item.layer }, { "buildId", item.area }, { "status", "" } },
-						Timestamp = DateTime.UtcNow
-					};
-					client.Write(commonDAO.appConfig.InfluxDbName, point_model);
-					#endregion
+						#region 直接进业务库
 
-					res++;
+						var usermodel = commonDAO.SelfDber.Entity<JCXXTBCOMPANYUSERINFOEXTEND>("where BEACON=:BEACON", new { BEACON = item.deviceNo });
+						Staffduty_Position staffduty_Position = new Staffduty_Position();
+						staffduty_Position.BuildId = item.area;
+						staffduty_Position.FloorNo = item.layer;
+						staffduty_Position.Name = item.empName;
+						staffduty_Position.Xcoor = item.astarPointList.LastOrDefault().longitude;
+						staffduty_Position.Ycoor = item.astarPointList.LastOrDefault().latitude;
+						staffduty_Position.Zcoor = commonDAO.appConfig.Zcoor;
+						staffduty_Position.LastUpdateTime = DateTime.Now;
+						staffduty_Position.Id = usermodel == null ? "" : usermodel.UserId;
+						Dbers.GetInstance().SelfDber.Insert(staffduty_Position);
+						#endregion
+
+						#region 存redis数据库
+						redis.HashSet(item.empName, "name", item.empName);
+						redis.HashSet(item.empName, "lastUpdateTime", DateTimeOffset.UtcNow.ToLocalTime());
+						redis.HashSet(item.empName, "x-Coor", staffduty_Position.Xcoor);
+						redis.HashSet(item.empName, "y-Coor", staffduty_Position.Ycoor);
+						redis.HashSet(item.empName, "floorNo", item.layer);
+						redis.HashSet(item.empName, "buildId", item.area);
+						redis.HashSet(item.empName, "status", "");
+						#endregion
+
+						#region 存时序数据库
+						var point_model = new Point()
+						{
+							Name = commonDAO.appConfig.InfluxDbLocationUserTableName,//表名
+							Tags = new Dictionary<string, object>() { { "userId", item.empName } },
+							Fields = new Dictionary<string, object>() { { "x-Coor", staffduty_Position.Xcoor }, { "y-Coor", staffduty_Position.Ycoor }, { "floorNo", item.layer }, { "buildId", item.area }, { "status", "" } },
+							Timestamp = DateTime.UtcNow
+						};
+						client.Write(commonDAO.appConfig.InfluxDbName, point_model);
+						#endregion
+
+						res++;
+					}
 				}
 			}
 			output(string.Format("同步实时定位人员{0} 条", res), eOutputType.Normal);
